9eca387bcfa7776e8ae343392f925a30bae89e32
el160 <ljhongeri04@gmail.com>
2024-12-20 00:05:25 +0300

    changes

--- myapp/my_club/players/models.py
+++ myapp/my_club/players/models.py
@@ -4,3 +4,9 @@ class Player(models.Model):
     firstname = models.CharField(max_length=255)  
     lastname = models.CharField(max_length=255)  
     age = models.IntegerField()  
+    phone = models.IntegerField(null=True)
+    date_joined = models.DateField(null=True)
+
+
+def __str__(self):
+        return f"{self.firstname}, {self.lastname}, Age: {self.age}"
