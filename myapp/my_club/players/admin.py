48a54546116f9189f7de7491b258446ce5a9e31c
el160 <ljhongeri04@gmail.com>
2024-12-23 12:01:56 +0300

    changes

--- myapp/my_club/players/admin.py
+++ myapp/my_club/players/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Player
 # Register your models here.
+#admin.site.register(Player)
+class PlayerAdmin(admin.ModelAdmin): # for displaying purpose
+    list_display = ('firstname','lastname','age')
+
+admin.site.register(Player, PlayerAdmin)
