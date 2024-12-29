cf961741757c7c8ff8c07682c6d27d0e4d7731aa
el160 <ljhongeri04@gmail.com>
2024-12-29 11:07:17 +0300

    changes

--- myapp/my_club/my_club/settings.py
+++ myapp/my_club/my_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_w2)*yr*ti+8d0ysef&vy9k&#2-k2hq%(18rc2l+tc*zckfu14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =  ['127.0.0.1', 'localhost']
 
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_club.urls'
@@ -117,9 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+STATICFILES_DIRS = [
+     BASE_DIR / "mystaticfiles"
+     ]
 
 
 # Default primary key field type
